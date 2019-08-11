0a7eabf63b936def03df60850be39f13cbe73360
James Irick <james.e.irick@hotmail.com>
2019-08-11 19:45:13 -0400

    wip

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -5,19 +5,20 @@ import {
   ElementRef,
   OnInit,
   OnDestroy, 
-  Inject
+  Inject,
+  PLATFORM_ID
 } from '@angular/core';
 import { WINDOW } from "@ng-toolkit/universal";
 import { HostListener } from "@angular/core";
 import { debounceTime } from "rxjs/operators";
 import { Subject } from 'rxjs/internal/Subject';
 
+import { isPlatformBrowser} from '@angular/common';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home-page.component.html',
-    styleUrls: ['./home-page.component.scss'],
-    providers: [{ provide: WINDOW, useValue: WINDOW }]
+    styleUrls: ['./home-page.component.scss']
   })
 
 export class HomePageComponent implements OnInit, OnDestroy, AfterViewInit {
@@ -25,7 +26,6 @@ export class HomePageComponent implements OnInit, OnDestroy, AfterViewInit {
   public isSticky: boolean = false;
   public copyInview: boolean = false;
   public connectInview: boolean = false;
-  public isBrowser: boolean = false;
 
   @ViewChild('copyContainer', {static: false}) copyContainer: ElementRef;
   @ViewChild('connectContainer', {static: false}) connectContainer: ElementRef;
@@ -35,7 +35,13 @@ export class HomePageComponent implements OnInit, OnDestroy, AfterViewInit {
   private connectContainerOffSetTop: number;
   private pageYOffsetSubject = new Subject<number>();
 
-  constructor(@Inject(WINDOW) private window: Window) {
+  constructor(@Inject(WINDOW) private window: Window, @Inject(PLATFORM_ID) private platformId) {
+
+    if (!isPlatformBrowser(platformId)) {
+      this.isSticky = true;
+      this.copyInview = true;
+      this.connectInview = true;
+    }
   }
 
   @HostListener("window:scroll", [])
@@ -44,10 +50,10 @@ export class HomePageComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   ngOnInit(): void {
-   /* this.scrollObservable = this.pageYOffsetSubject
+    this.scrollObservable = this.pageYOffsetSubject
       .asObservable()
       .pipe(debounceTime(10))
-      .subscribe((e:number) => this.onWindowScrollEvent(e))*/
+      .subscribe((e:number) => this.onWindowScrollEvent(e));
   }
 
   ngOnDestroy(): void {
